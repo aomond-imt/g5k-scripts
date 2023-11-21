fdddf8463437fc4a6cb847f3c3ecc25a1ef35f75
aomond <antoine.omond@imt-atlantique.fr>
2023-11-21 18:15:48 +0100

    first commit

---
+++ reservation.sh
@@ -0,0 +1,2 @@
+oarsub -l host=5,walltime=8 -p gros -r '2023-11-21 19:00:00'
+
---
+++ stop_expes_on_nodes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
+for host in $hosts; do
+  host_split=($(echo "$host" | tr "." " "))
+  host_node=${host_split[0]}
+  echo "killing tmux session on host $host_node"
+  tmux kill-session -t "$host_node"
+  ssh -t "$host_node" "kill $(ps -aux | pgrep -f esds)"
+done
